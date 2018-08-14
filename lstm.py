a9ae3886563711ec649be735c51ce41fbf135e41
andresmaca <mhandres@unicauca.edu.co>
2018-08-13 23:17:47 -0500

    wii

--- lstm.py
+++ lstm.py
@@ -17,7 +17,11 @@ def read_words(filename):
 def build_vocab(filename):
     data = read_words(filename)
     counter = collections.Counter(data)
+    print("first counter items: "+counter.items()[0])
     count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+    """
+    key is a function that will be called to transform the collection's items before they are compared. The parameter passed to key must be something that is callable.
+    """
     print("sorted: "+count_pairs[:4])
     words, _ = list(zip(*count_pairs))
     """
@@ -29,4 +33,30 @@ def build_vocab(filename):
     """
     word_to_id = dict(zip(words, range(len(words))))
     return word_to_id
-sdsad
+
+def file_to_word_ids(filename, word_to_id):
+    data = read_words(filename)
+    return [word_to_id[word] for word in data if word in word_to_id]
+
+def load_data():
+    train_path = os.path.join(data_path, "ptb.train.txt")
+    valid_path = os.path.join(datapath, "ptb.valid.txt")
+    test_path = os.path.join(datapath, "ptb.test.txt")
+
+    word_to_id = build_vocab(train_path)
+    train_data = file_to_word_ids(train_path, word_to_id)
+    valid_data = file_to_word_ids(valid_path, word_to_id)
+    test_data = file_to_word_ids(test_path, word_to_id)
+    vocabulary = len(word_to_id)
+    reversed_dictionary = dict(zip(word_to_id.values(), word_to_id.keys()))
+    """
+    x = (1,2,3)
+    y = (a,b,c)
+    zip(x,y)
+    >>(1,a),(2,b),(3,c)
+    """
+    print(train_data[:5])
+    print(word_to_id)
+    print(vocabulary)
+    print(" ".join([reversed_dictionary[x] for x in train_data[:10]]))
+    return train_data, valid_data, test_data, vocabulary, reversed_dictionary
